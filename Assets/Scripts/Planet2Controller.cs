12cf74cc8f48b72579846106b8e6297e9a57bb0c
Adriana Vecc <adriana.inbox+github@gmail.com>
2017-01-02 11:19:03 +0100

    Adding gaze interactions and event system

---
+++ Assets/Scripts/Planet2Controller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Planet2Controller : MonoBehaviour {
+	public Vector3 lastPosition;
+
+	// Use this for initialization
+	void Start () {
+		lastPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	public void LookAtPlanet2 () {
+		lastPosition = new Vector3(lastPosition.x, lastPosition.y + 0.5f, lastPosition.z + 0.5f);
+		transform.position = lastPosition;
+	}
+}
