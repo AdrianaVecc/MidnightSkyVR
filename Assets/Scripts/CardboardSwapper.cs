4699967589b736c772bb6aa736ead05b25b24a73
Adriana Vecc <adriana.inbox+github@gmail.com>
2017-01-05 01:43:50 +0000

    Implemented @aornerlas nifty VR switch

---
+++ Assets/Scripts/CardboardSwapper.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardboardSwapper : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+		this.gameObject.transform.Find("Canvas/CardboardButton").gameObject.SetActive(!GvrViewer.Instance.VRModeEnabled);
+		if (GvrViewer.Instance.Tilted || GvrViewer.Instance.BackButtonPressed) 
+		{
+			DisableVR();
+		}
+	
+	}
+
+	public void DisableVR ()
+	{
+		GvrViewer.Instance.VRModeEnabled = false;
+	}
+
+	public void EnableVR ()
+	{
+		GvrViewer.Instance.VRModeEnabled = true;
+	}
+}
