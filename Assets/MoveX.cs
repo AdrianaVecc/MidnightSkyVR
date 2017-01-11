0b4bedea941770a84b35f95bb3c47aa23fc47b5e
Adriana Vecc <adriana.inbox+github@gmail.com>
2017-01-11 11:01:36 +0000

    Fog in dead corners + Planet orbit + materials for comets

---
+++ Assets/MoveX.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveX : MonoBehaviour {
+
+	public float speed;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (new Vector3 (speed, 0, 0) * Time.deltaTime);
+	}
+}
