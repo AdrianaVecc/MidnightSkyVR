b1936a771841e816603c802326fa5c00295543fc
Adriana Vecc <adriana.inbox+github@gmail.com>
2017-07-05 17:04:01 -0700

    Dinamycally create sparkling stars

---
+++ Assets/Scripts/ObjectMaker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMaker : MonoBehaviour {
+
+	public GameObject spaceElement;
+
+	// Use this for initialization
+	void Start () {
+		for (int x = 0; x < 5; x++) {
+			for (int y = 0; y < 5; y++) {
+				for (int z = 0; z < 5; z++)  {
+					Object.Instantiate (spaceElement, new Vector3 (20 + 15 * (x + Random.value), 20 + 15 * (y + Random.value), 20 + 15 * (z + Random.value)), Quaternion.identity);
+				}
+			}
+		}
+	}
+	
+}
