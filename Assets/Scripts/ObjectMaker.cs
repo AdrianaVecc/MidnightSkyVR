b35b0c59d9bfb43fb8daad812ab8cd6bd1285f11
Adriana Vecc <adriana.inbox+github@gmail.com>
2017-07-05 21:58:52 -0700

    Object Maker script places stars

--- Assets/Scripts/ObjectMaker.cs
+++ Assets/Scripts/ObjectMaker.cs
@@ -5,14 +5,37 @@ using UnityEngine;
 public class ObjectMaker : MonoBehaviour {
 
 	public GameObject spaceElement;
+	public int maxNumberOfElements;
+	public int distanceFromOrigin;
+	public int step;
 
-	// Use this for initialization
+	private float randx;
+	private float randy;
+	private float randz;
+
+	private float rx;
+	private float ry;
+	private float rz;
+
 	void Start () {
-		for (int x = 0; x < 5; x++) {
-			for (int y = 0; y < 5; y++) {
-				for (int z = 0; z < 5; z++)  {
-					Object.Instantiate (spaceElement, new Vector3 (20 + 15 * (x + Random.value), 20 + 15 * (y + Random.value), 20 + 15 * (z + Random.value)), Quaternion.identity);
-				}
+		for (int x = 0; x < Mathf.Round(Mathf.Pow(maxNumberOfElements, 1f / 3f)); x++) {
+			for (int y = 0; y < Mathf.Round(Mathf.Pow(maxNumberOfElements, 1f / 3f)); y++) {
+				for (int z = 0; z < Mathf.Round(Mathf.Pow(maxNumberOfElements, 1f / 3f)); z++)  {
+
+					randx = (Random.value < 0.5) ? 1 : -1;
+					randy = (Random.value < 0.5) ? 1 : -1;
+					randz = (Random.value < 0.5) ? 1 : -1;
+
+					rx = randx * (distanceFromOrigin + step * (x + Random.value));
+					ry = randy * (distanceFromOrigin + step * (y + Random.value));
+					rz = randz * (distanceFromOrigin + step * (z + Random.value));
+
+					Vector3 position = new Vector3 (rx, ry, rz);
+					Object.Instantiate (spaceElement, position, Quaternion.identity);
+
+					Debug.Log (rx);
+
+						}
 			}
 		}
 	}
--- Assets/Scripts/Planet2Controller.cs
+++ Assets/Scripts/Planet2Controller.cs
@@ -4,12 +4,10 @@ using System.Collections;
 public class Planet2Controller : MonoBehaviour {
 	public Vector3 lastPosition;
 
-	// Use this for initialization
 	void Start () {
 		lastPosition = transform.position;
 	}
 		
-	// Update is called once per frame
 	public void LookAtPlanet2 () {
 		lastPosition = new Vector3(lastPosition.x, lastPosition.y + 0.1f, lastPosition.z + 0.1f);
 		transform.position = lastPosition;
